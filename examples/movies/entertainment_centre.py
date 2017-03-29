86b7d77bf814a072169ff0c520c1c102a27223d8
helenypan <yue.pan@me.com>
2017-03-29 23:46:50 +0100

    add movies class example

---
+++ examples/movies/entertainment_centre.py
@@ -0,0 +1,8 @@
+import media
+
+toy_Story = media.Movie("Toy Story",
+	"A story of a boy and his torys that come to lie",
+	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	"https://www.youtube.com/watch?v=vwyZH85NQC4")
+print(toy_Story.storyline)
+toy_Story.show_trailer()
